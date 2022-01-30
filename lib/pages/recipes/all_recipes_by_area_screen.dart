740754767176d75700b56dd63207fe689c03af8c
Jeff <Jeff59550@gmail.com>
2022-01-30 20:48:26 +0100

    :bug: :sparkles: : fix area get

---
+++ lib/pages/recipes/all_recipes_by_area_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class AllRecipesByAreaScreen extends StatelessWidget {
+  const AllRecipesByAreaScreen({Key? key, this.areas}) : super(key: key);
+  final areas;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
